df689d63797ace03a823603487bb12410b4f2b7f
Mykhailo Hetmanskyi <mykhailo.hetmanskyi@chisw.com>
2021-11-14 00:29:28 +0200

    changed logic for setuping xcodegen project.yml added setup.sh script for generating project.yml with new parameters

---
+++ setup.sh
@@ -0,0 +1,18 @@
+project_dir=$(pwd)
+project_name="PROJECT_NAME"
+echo 'Enter project name'
+read project_name
+
+git clone -b feature/Xcodegen https://github.com/hetmanskiy/MVP-Coordinator.git ./$project_name
+
+sed -i -e 's#\$PROJECT_NAME\$#'$project_name'#g' $project_name/project.yml
+
+sed -i -e 's#\$PROJECT_PATH\$#'$project_dir/$project_name'#g' $project_name/project.yml
+
+sed -i -e 's#\$PROJECT_NAME\$#'$project_name'#g' $project_name/Podfile
+
+rm $project_name/project.yml-e
+
+cd ./$project_name
+xcodegen generate
+
